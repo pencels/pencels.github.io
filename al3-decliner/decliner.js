902e62100edf8bb88301f6f03a3880dd2929bfb6
Chris Pence <6cpence@gmail.com>
2016-08-16 12:19:51 -0700

    Added decliner to site.

---
+++ al3-decliner/decliner.js
@@ -0,0 +1,326 @@
+var e_um = String.fromCharCode(235);
+var n_pal = String.fromCharCode(324);
+var s_pal = String.fromCharCode(347);
+var s_alv = String.fromCharCode(349);
+var c_pal = String.fromCharCode(263);
+var c_alv = String.fromCharCode(265);
+var g_pal = String.fromCharCode(501);
+var g_alv = String.fromCharCode(285);
+
+function decline() {
+    var noun;
+    var isnoun = true;
+
+    //Get noun from input
+    noun = document.getElementById("noun-input").value;
+
+    var ending = noun.slice(-1);
+    console.log(ending.charCodeAt(0));
+
+    if (isVowel(ending)) {
+        console.log("vowel");
+        declineVowel(noun);
+    } else if (isNasalApprox(ending)) {
+        console.log("approx");
+        declineNasalApprox(noun);
+    } else if (isFricative(ending)) {
+        console.log("fricative");
+        declineFricative(noun);
+    } else if (isAffricate(ending)) {
+        console.log("affricate");
+        declineAffricate(noun);
+    } else {
+        isnoun = false;
+        alert("The given word is not a noun.");
+    }
+
+    if (isnoun)
+        document.getElementById("results").innerHTML = "Declension of <b>" +
+                                                    noun + "</b> in Alh" +
+                                                    e_um + ":";
+}
+
+function loadInputButtons() {
+    var list = document.getElementsByClassName('special-char');
+    for (var i = 0; i < list.length; i++) {
+        list[i].setAttribute('onmousedown', 'inputLetter(this)');
+        list[i].setAttribute('onmouseup', 'resetButton(this)');
+    }
+}
+
+function inputLetter(elem) {
+    document.getElementById("noun-input").value += elem.value;
+    elem.style.background = "#47C2BE";
+}
+
+function resetButton(elem) {
+    document.getElementById("noun-input").focus();
+    elem.style.background = "#287775";
+}
+
+function isVowel(letter) {
+    var code = letter.charCodeAt();
+    if (code == 97 || code == 101 || code == 105
+        || code == 111 || code == 117 || code == 235)
+        return true;
+    else
+        return false;
+}
+
+function isNasalApprox(letter) {
+    if (letter == "m" || letter == "n" || letter == "r")
+        return true;
+    else
+        return false;
+}
+
+function isFricative(letter) {
+    var code = letter.charCodeAt();
+    if (code == 115 || code == 349 || code == 347)
+        return true;
+    else
+        return false;
+}
+
+function isAffricate(letter) {
+    var code = letter.charCodeAt();
+    if (code == 263|| code == 265 || code == 285 || code == 501)
+        return true;
+    else
+        return false;
+}
+
+//accepts an array of 8 elements for the sg and pl forms of each case
+function display(array) {
+    document.getElementById("nomsg").innerHTML = array[0];
+    document.getElementById("nompl").innerHTML = array[1];
+    document.getElementById("accsg").innerHTML = array[2];
+    document.getElementById("accpl").innerHTML = array[3];
+    document.getElementById("gensg").innerHTML = array[4];
+    document.getElementById("genpl").innerHTML = array[5];
+    document.getElementById("oblsg").innerHTML = array[6];
+    document.getElementById("oblpl").innerHTML = array[7];
+    document.getElementById("decl-table").style.display = "inline-block";
+}
+
+function vowelChg(vowel, type) {
+    if (vowel.charCodeAt() == 235)
+        vowel = "e:";
+
+    if (type == "NOM V") {
+        switch (vowel) {
+            case "a":
+                return "e";
+            case "e":
+                return "er";
+            case "e:":
+                return e_um + "a";
+            case "i":
+                return "ei";
+            case "o":
+                return "a";
+            case "u":
+                return "au";
+            default:
+                return ""
+        }
+    } else if (type == "GEN V") {
+        switch (vowel) {
+            case "a":
+                return "e";
+            case "e":
+                return "e";
+            case "e:":
+                return "a";
+            case "i":
+                return "e";
+            case "o":
+                return "a";
+            case "u":
+                return "a";
+            default:
+                return ""
+        }
+    } else if (type == "NOM FR") {
+        switch (vowel) {
+            case "a":
+                return "ai";
+            case "e":
+                return "i";
+            case "e:":
+                return e_um + "a";
+            case "i":
+                return "ei";
+            case "o":
+                return "a";
+            case "u":
+                return "uo";
+            default:
+                return ""
+        }
+    }
+}
+
+function declineVowel(n) {
+    var results = [];
+    var end = n.slice(-1);
+    //nom sg
+    results[0] = n;
+
+    //nom pl
+    results[1] = n.slice(0,-1) + vowelChg(end, "NOM V");
+
+    //acc sg
+    results[2] = n + "c";
+
+    //acc pl
+    if (n.slice(-2) == "na" || n.charAt(n.length-2) == n_pal + "a")
+        results[3] = n.slice(0,-1) + "na";
+    else
+        results[3] = n + "na";
+
+
+    //gen sg
+    results[4] = n.slice(0,-1) + vowelChg(end, "GEN V") + "m";
+
+    //gen pl
+    results[5] = n.slice(0,-1) + vowelChg(end, "GEN V") + "m" + end;
+
+    //obl sg
+    results[6] = n + "s";
+
+    //obl pl
+    results[7] = n + "f";
+
+    display(results);
+}
+function declineNasalApprox(n) {
+    var results = [];
+    var end = n.slice(-1);
+    //nom sg
+    results[0] = n;
+
+    //nom pl
+    results[1] = n + "i";
+
+    //acc sg
+    if (end == "r")
+        results[2] = n.slice(0,-2) + "r" + n.charAt(n.length-2) + "c";
+    else
+        results[2] = n + "ec";
+
+    //acc pl
+    if (end == "m")
+        results[3] = n + "ma";
+    else
+        results[3] = n + "na";
+
+    //gen sg
+    if (end == "m")
+        results[4] = n + "ba";
+    else if (end == "n")
+        results[4] = n + "da";
+    else
+        results[4] = n + "da";
+
+    //gen pl
+    if (end == "m")
+        results[5] = n + "mu";
+    else
+        results[5] = n + "emu";
+
+    //obl sg
+    if (end == "r")
+        results[6] = n.slice(0,-2) + "r" + n.charAt(n.length-2) + "s";
+    else
+        results[6] = n + "as";
+
+    //obl pl
+    results[7] = n + "fa";
+
+    display(results);
+}
+function declineFricative(n) {
+    var results = [];
+    var end = n.slice(-1);
+    //nom sg
+    results[0] = n;
+
+    //nom pl
+    results[1] = n.slice(0,-2) + vowelChg(n.charAt(n.length-2), "NOM FR") + end;
+
+    //acc sg
+    results[2] = n + "de";
+
+    //acc pl
+    results[3] = n + "na";
+
+    //gen sg
+    var syllonset = n.charAt(n.length-3);
+    var combo = "";
+    if (end == s_pal) {
+        if (syllonset == "t")
+            combo = c_pal;
+        else if (syllonset == "d")
+            combo = g_pal;
+        else if (isVowel(syllonset) || isFricative(syllonset) || isAffricate(syllonset))
+            combo = syllonset + end;
+        else
+            combo = syllonset + n.charAt(n.length-2)+ end;
+    } else if (end == s_alv) {
+        if (syllonset == "t")
+            combo = c_alv;
+        else if (syllonset == "d")
+            combo = g_alv;
+        else if (isVowel(syllonset) || isFricative(syllonset) || isAffricate(syllonset))
+            combo = syllonset + end;
+        else
+            combo = syllonset + n.charAt(n.length-2)+ end;
+    } else {
+        if (isVowel(syllonset) || isFricative(syllonset) || isAffricate(syllonset))
+            combo = syllonset + end;
+        else
+            combo = syllonset + n.charAt(n.length-2) + end;
+    }
+    results[4] = n.slice(0,-3) + combo + n.charAt(n.length-2) + "m";
+
+    //gen pl
+    results[5] = n.slice(0,-3) + combo + n.charAt(n.length-2) + "da";
+
+    //obl sg
+    results[6] = n;
+
+    //obl pl
+    results[7] = n + "of";
+
+    display(results);
+}
+function declineAffricate(n) {
+    var results = [];
+    var end = n.slice(-1);
+    //nom sg
+    results[0] = n;
+
+    //nom pl
+    results[1] = n.slice(0,-2) + vowelChg(n.charAt(n.length-2), "NOM FR") + end;
+
+    //acc sg
+    results[2] = n + "e";
+
+    //acc pl
+    results[3] = n + "ina";
+
+    //gen sg
+    results[4] = n + "em";
+
+    //gen pl
+    results[5] = n + "emu";
+
+    //obl sg
+    results[6] = n + "es";
+
+    //obl pl
+    results[7] = n + "of";
+
+    display(results);
+}
