a83e59acef1900f4bbde644d7a1a9042cb90dc07
Chris Pence <6cpence@gmail.com>
2016-08-24 16:11:53 -0700

    Updated dining calculator for 16-17 year.

--- dcalc/dining.js
+++ dcalc/dining.js
@@ -1,28 +1,35 @@
 // ranges are inclusive of the first date and exclusive of the second
 // [first, second)
-const YEAR_START = new Date(2015, 8, 19); // start and end of dining plan term
-const YEAR_END = new Date(2016, 5, 12);
+// Months start from 0!
+const YEAR_START = new Date(2016, 8, 17); // start and end of dining plan term
+const YEAR_END = new Date(2017, 5, 18);
 
-const TH_BREAK_START = new Date(2015, 10, 26);
-const TH_BREAK_END = new Date(2015, 10, 28);
+// Thanksgiving break
+const TH_BREAK_START = new Date(2016, 10, 24);
+const TH_BREAK_END = new Date(2016, 10, 26);
 
-const W_BREAK_START = new Date(2015, 11, 13);
-const W_BREAK_END = new Date(2016, 0, 3);
+// Winter break
+const W_BREAK_START = new Date(2016, 11, 10);
+const W_BREAK_END = new Date(2017, 0, 7);
 
-const S_BREAK_START = new Date(2016, 2, 20);
-const S_BREAK_END = new Date(2016, 2, 27);
+// Spring break
+const S_BREAK_START = new Date(2017, 2, 25);
+const S_BREAK_END = new Date(2017, 3, 1);
 
 const breaks = [TH_BREAK_START, TH_BREAK_END,
    W_BREAK_START, W_BREAK_END,
    S_BREAK_START, S_BREAK_END
 ];
 
-const MS_PER_DAY = 1000 * 60 * 60 * 24;
+const MS_PER_DAY = 1000 * 60 * 60 * 24; // milliseconds per day
 const DINING_DAYS = nonHolidaysBetween(YEAR_START, YEAR_END);
 
 function budgetOf(plan, balance) {
    var today = new Date();
    var duration = nonHolidaysBetween(YEAR_START, today);
+
+   // Don't count days if year hasn't started yet
+   if (duration < 0) duration = 0;
 
    var spent = plan - balance;
    var expected = (plan / DINING_DAYS) * duration;
